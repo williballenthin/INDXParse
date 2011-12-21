3e43fe825d96116bc3748d4df4dfa266a6444d0c
Willi Ballenthin <willi.ballenthin@gmail.com>
2011-12-20 21:02:47 -0500

    backup of IndexEntry

--- MFTINDX.py
+++ MFTINDX.py
@@ -42,6 +42,35 @@ def align(offset, alignment):
         return offset
         return offset + (alignment - (offset % alignment))
 
+def dosdate(dosdate, dostime):
+    """
+    `dosdate`: 2 bytes, little endian.
+    `dostime`: 2 bytes, little endian.
+    returns: datetime.datetime or datetime.datetime.min on error
+    """
+    try:
+        t  = ord(dosdate[1]) << 8
+        t |= ord(dosdate[0])
+        day   = t & 0b0000000000011111
+        month = (t & 0b0000000111100000) >> 5
+        year  = (t & 0b1111111000000000) >> 9
+        year += 1980
+        
+        t  = ord(dostime[1]) << 8
+        t |= ord(dostime[0])
+        sec     = t & 0b0000000000011111
+        sec    *= 2
+        minute  = (t & 0b0000011111100000) >> 5
+        hour    = (t & 0b1111100000000000) >> 11
+
+        return datetime.datetime(year, month, day, hour, minute, sec)
+    except:
+        return datetime.datetime.min
+
+def parse_windows_timestamp(qword):
+    # see http://integriography.wordpress.com/2010/01/16/using-phython-to-parse-and-present-windows-64-bit-timestamps/
+    return datetime.utcfromtimestamp(float(qword) * 1e-7 - 11644473600)
+
 class INDXException(Exception):
     """
     Base Exception class for INDX parsing.
@@ -153,6 +182,12 @@ class Block(object):
             self._implicit_offset = offset + 8
         elif type == "int":
             self._implicit_offset = offset + 4
+        elif type == "dosdate":
+            self._implicit_offset = offset + 4
+        elif type == "windows_timestamp":
+            self._implicit_offset = offset + 8
+        elif type == "binary" and length:
+            self._implicit_offset = offset + length
         elif type == "string" and length:
             self._implicit_offset = offset + length
         elif type == "wstring" and length:
@@ -161,6 +196,9 @@ class Block(object):
             raise INDXException("Implicit offset not supported for dynamic length strings")
         else:
             raise INDXException("Implicit offset not supported for type: " + type)            
+
+    def current_field_offset(self):
+        return self._implicit_offset
 
     def unpack_byte(self, offset):
         """
@@ -242,6 +280,21 @@ class Block(object):
         except struct.error:
             raise OverrunBufferException(o, len(self._buf))
 
+    def unpack_binary(self, offset, length):
+        """
+        Returns raw binary data from the relative offset with the given length.
+        Arguments:
+        - `offset`: The relative offset from the start of the block.
+        - `length`: The length of the binary blob.
+        Throws:
+        - `OverrunBufferException`
+        """
+        o = self._offset + offset
+        try:
+            return struct.unpack_from("<%ds" % (length), self._buf, o)[0]
+        except struct.error:
+            raise OverrunBufferException(o, len(self._buf))
+
     def unpack_string(self, offset, length):
         """
         Returns a string from the relative offset with the given length.
@@ -251,11 +304,7 @@ class Block(object):
         Throws:
         - `OverrunBufferException`
         """
-        o = self._offset + offset
-        try:
-            return struct.unpack_from("<%ds" % (length), self._buf, o)[0]
-        except struct.error:
-            raise OverrunBufferException(o, len(self._buf))
+        return self.unpack_binary(offset, length)
 
     def unpack_wstring(self, offset, length):
         """
@@ -268,6 +317,32 @@ class Block(object):
         - `UnicodeDecodeError`
         """
         return self._buf[self._offset + offset:self._offset + offset + 2 * length].tostring().decode("utf16")
+
+    def unpack_dosdate(self, offset):
+        """
+        Returns a datetime from the DOSDATE and DOSTIME starting at 
+        the relative offset.
+        Arguments:
+        - `offset`: The relative offset from the start of the block.
+        Throws:
+        - `OverrunBufferException`
+        """
+        try:
+            o = self._offset + offset
+            return dosdate(self._buf[o:o + 2], self._buf[o + 2:o + 4])
+        except struct.error:
+            raise OverrunBufferException(o, len(self._buf))
+
+    def unpack_windows_timestamp(self, offset):
+        """
+        Returns a datetime from the QWORD Windows timestamp starting at
+        the relative offset.
+        Arguments:
+        - `offset`: The relative offset from the start of the block.
+        Throws:
+        - `OverrunBufferException`
+        """
+        return parse_windows_timestamp(self.unpack_qword(offset))
 
     def absolute_offset(self, offset):
         """
@@ -311,6 +386,218 @@ class FixupBlock(Block):
             debug("Fixup verified at %s and patched from %s to %s." % (hex(self.offset() + fixup_offset),
                                                                        hex(fixup_value), hex(check_value)))
 
+class IndexRootHeader(Block):
+    def __init__(self, buf, offset, parent):
+        debug("INDEX ROOT HEADER at %s." % (hex(offset)))
+        super(IndexRootHeader, self).__init__(buf, offset, parent)
+        self.declare_field("dword", "type", 0x0)
+        self.declare_field("dword", "collation_rule")
+        self.declare_field("dword", "index_record_size_bytes")
+        self.declare_field("byte",  "index_record_size_clusters")
+        self.declare_field("dword", "unused")
+
+class IndexNodeHeader(Block):
+    def __init__(self, buf, offset, parent):
+        debug("INDEX NODE HEADER at %s." % (hex(offset)))
+        super(IndexNodeHeader, self).__init__(buf, offset, parent)
+        self.declare_field("dword", "entry_list_start", 0x0)
+        self.declare_field("dword", "entry_list_end")
+        self.declare_field("dword", "entry_list_allocation_end")
+        self.declare_field("dword", "flags")
+        self.declare_field("binary", "list_buffer", self.entry_list_start(), self.entry_list_allocation_end() - self.entry_list_start())
+
+class IndexEntry2(Block):
+    def __init__(self, buf, offset, parent):
+        debug("INDEX ENTRY at %s." % (hex(offset)))
+        super(IndexEntry2, self).__init__(buf, offset, parent)
+        self.declare_field("qword", "mft_reference", 0x0)
+        self.declare_field("word", "length")
+        self.declare_field("word", "filename_information_length")
+        self.declare_field("dword", "flags")
+        if self.filename_information_length() > 0:
+            self.declare_field("binary", "filename_information_buffer", self.current_field_offset(), self.filename_information_length())
+        self.declare_field("qword", "child_vcn", align(self.current_field_offset(), 0x8))
+
+class FilenameAttribute(Block):
+    def __init__(self, buf, offset, parent):
+        debug("Filename attribute at %s." % (hex(offset)))
+        super(FilenameAttribute, self).__init__(buf, offset, parent)
+        self.declare_field("qword", "mft_reference", 0x0)
+        self.declare_field("word", "length")
+        self.declare_field("word", "filename_information_length")
+        self.declare_field("dword", "flags")
+        if self.filename_information_length() > 0:
+            self.declare_field("binary", "filename_information_buffer", self.current_field_offset(), self.filename_information_length())
+            self.declare_field("qword", "child_vcn", align(self.current_field_offset(), 0x8))
+    
+
+class IndexEntry(Block):
+    def __init__(self, buf, offset, parent):
+        """
+        Constructor.
+        Arguments:
+        - `buf`: Byte string containing NTFS INDX file
+        - `offset`: The offset into the buffer at which the block starts.
+        - `parent`: The parent NTATTR_STANDARD_INDEX_HEADER block, which links to this block.
+        """
+        debug("INDEX ENTRY at %s." % (hex(offset)))
+        super(IndexEntry, self).__init__(buf, offset, parent)
+
+        self.declare_field("windows_timestamp", "created_time", 0x18)
+        self.declare_field("windows_timestamp", "modified_time")
+        self.declare_field("windows_timestamp", "changed_time")
+        self.declare_field("windows_timestamp", "accessed_time")
+        self.declare_field("qword", "physical_size")
+        self.declare_field("qword", "logical_size")
+
+        self.declare_field("byte", "filename_length", 0x50)
+        self.declare_field("byte", "filename_type")
+        self.declare_field("wstring", "filename", 0x52, self.filename_length()) #TODO check this
+
+        if self.filename_type() > 4:
+            warning("Invalid INDX record entry filename type at 0x%s" % (hex(self.offset() + self._filename_type_offset)))
+
+    def end_offset(self):
+        """
+        return the first address not a part of this block
+        """
+        string_end = self.offset() + self._filename_offset + 2 * self.filename_length()
+        return align(string_end, 8)
+
+    def has_next(self):
+        return self.end_offset() - self.parent().offset() <= self.parent().entry_size()
+        
+    def next(self):
+        """
+        return the next entry after this one.
+        warning, this does not check to see if another exists, but blindly creates one
+        from the next data in the buffer. check NTATTR_STANDARD_INDEX_ENTRY.has_next() first
+        """
+        return IndexEntry(self._buf, self.end_offset(), self.parent())
+
+    def parse_time_safe(self, offset):
+        """
+        The *_safe time methods return the date of the
+        UNIX epoch if there is an exception parsing the 
+        date
+        """
+        try:
+            return self.parse_time(offset)
+        except ValueError:
+            debug("Timestamp is invalid, using a default.")
+            return datetime(1970, 1, 1, 0, 0, 0)
+
+    def created_time_safe(self):
+        return self.parse_time_safe(self._created_time_offset)
+
+    def modified_time_safe(self):
+        return self.parse_time_safe(self._modified_time_offset)
+        
+    def changed_time_safe(self):
+        return self.parse_time_safe(self._changed_time_offset)
+
+    def accessed_time_safe(self):
+        return self.parse_time_safe(self._accessed_time_offset)
+
+class SlackIndexEntry(IndexEntry):
+    def __init__(self, buf, offset, parent):
+        """
+        Constructor.
+        Arguments:
+        - `buf`: Byte string containing NTFS INDX file
+        - `offset`: The offset into the buffer at which the block starts.
+        - `parent`: The parent NTATTR_STANDARD_INDEX_HEADER block, which links to this block.
+        """
+        super(SlackIndexEntry, self).__init__(buf, offset, parent)
+
+    def is_valid(self):
+        recent_date = datetime(1990, 1, 1, 0, 0, 0)
+        return self.modified_time_safe() > recent_date and \
+            self.accessed_time_safe() > recent_date and \
+            self.changed_time_safe() > recent_date and \
+            self.created_time_safe() > recent_date
+
+class IndexEntryHeader(FixupBlock):
+    def __init__(self, buf, offset, parent):
+        """
+        Constructor.
+        Arguments:
+        - `buf`: Byte string containing NTFS INDX file
+        - `offset`: The offset into the buffer at which the block starts.
+        - `parent`: The parent block, which links to this block.
+        """
+        debug("INDX HEADER @ %s." % (hex(offset)))
+        super(IndexEntryHeader, self).__init__(buf, offset, parent)
+
+        _magic = self.unpack_string(0, 4)
+        if _magic != "INDX":
+            raise ParseException("Invalid INDX ID")
+
+        self.declare_field("dword", "entry_size", 0x1C)
+        self.declare_field("dword", "allocated_size")
+
+        self.fixup(self.unpack_word(0x6), 0x28)
+
+    def entry_offset(self):
+        string_end = self.offset() + 0x2A + 2 * self.unpack_word(0x6)
+        return align(string_end, 8)
+
+    def entries(self):
+        """
+        A generator that returns each INDX entry associated with this header.
+        """
+        if self.entry_offset() - self.offset()  >= self.entry_size():
+            debug("No entries in this allocation block.")
+            return 
+
+        e = IndexEntry(self._buf, self.entry_offset(), self)
+        yield e
+
+        while e.has_next():
+            debug("Entry has another entry after it.")
+            e = e.next()
+            yield e
+            debug("No more entries.")
+
+    def slack(self):
+        return self._buf[self.offset() + self.entry_size():self.offset() + self.entry_allocated_size()]
+        
+    def end_offset(self):
+        return self.offset() + self.entry_allocated_size()
+
+    def deleted_entries(self):
+        """
+        A generator that yields INDX entries found in the slack space
+        associated with this header.
+        """
+        off = self.offset() + self.entry_size()
+
+        try:
+            while off < self.offset() + self.entry_allocated_size() - 0x52:
+                try:
+                    debug("Trying to find slack entry at %s." % (hex(off)))
+                    e = SlackIndexEntry(self._buf, off, self)
+                    if e.is_valid():
+                        debug("Slack entry is valid.")
+                        off = e.end_offset()
+                        yield e
+                    else:
+                        debug("Slack entry is invalid.")
+                        raise ParseException("Not a deleted entry")
+                except ParseException:
+                    debug("Scanning one byte forward.")
+                    off += 1
+        except struct.error:
+            debug("Slack entry parsing overran buffer.")
+            pass
+
+class ATTR_TYPE:
+    STANDARD_INFORMATION = 0x10
+    FILENAME_INFORMATION = 0x30
+    DATA = 0x80
+    INDEX_ROOT = 0x90
+    INDEX_ALLOCATION = 0xA0
+
 class Attribute(Block):
     def __init__(self, buf, offset, parent):
         super(Attribute, self).__init__(buf, offset, parent)
@@ -341,12 +628,10 @@ class Attribute(Block):
             self.declare_field("word", "value_offset")
             self.declare_field("byte", "value_flags")
             self.declare_field("byte", "reserved")
-
+            self.declare_field("binary", "value", self.value_offset(), self.value_length())
+            
     def runlist(self):
         return Runlist(buf, self.runlist_offset(), self)
-
-    def value(self):
-        return self._buf[self.value_offset():self.value_offset() + self.value_length()]
 
     def size(self):
         s = self.unpack_dword(self._size_offset) 
@@ -380,6 +665,11 @@ class MFTRecord(FixupBlock):
             offset += a.size()
             yield a
 
+    def attribute(self, attr_type):
+        for a in self.attributes():
+            if a.type() == attr_type:
+                return a
+
 def record_generator(filename):
     with open(filename, "rb") as f:
         record = f.read(1024)
@@ -393,10 +683,13 @@ def mft_get_record_buf(filename, number):
         return array.array("B", f.read(1024))
 
 def doit(filename, directory):
-    buf = mft_get_record_buf(filename, 0)
+    buf = mft_get_record_buf(filename, 5)
     m = MFTRecord(buf, 0, False)
-    for a in m.attributes():
-        pass
+    indx = m.attribute(ATTR_TYPE.INDEX_ROOT)
+    h = IndexEntryHeader(indx._buf, indx.absolute_offset(0x0), False)
+    for e in h.entries():
+        print e.filename()
+
 
     # buf = mft_get_record_buf(filename, 1)
     # m = MFTRecord(buf, 0, False)
