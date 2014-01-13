5b994a2ad13f901dad7ed56b873f7c648d6d340a
Willi Ballenthin <william.ballenthin@mandiant.com>
2014-01-13 18:36:55 -0500

    simplify extract_mft_record_slack, add license.

--- extract_mft_record_slack.py
+++ extract_mft_record_slack.py
@@ -1,24 +1,33 @@
+#!/bin/python
+
+#    This file is part of INDXParse.
+#
+#   Copyright 2014 Will Ballenthin <william.ballenthin@mandiant.com>
+#                    while at FireEye <http://www.FireEye.com>
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+#
 import sys
 
 from BinaryParser import Mmap
 from MFT import MFTEnumerator
-from MFT import Cache
-from MFT import ATTR_TYPE
-from MFT import MREF
-from MFT import IndexRootHeader
-from MFT import StandardInformationFieldDoesNotExist
 
 
 def main():
     filename = sys.argv[1]
 
     with Mmap(filename) as buf:
-        record_cache = Cache(1024)
-        path_cache = Cache(1024)
-
-        enum = MFTEnumerator(buf,
-                             record_cache=record_cache,
-                             path_cache=path_cache)
+        enum = MFTEnumerator(buf)
         for record in enum.enumerate_records():
             slack = record.slack()
             sys.stdout.write("\x00" * (1024 - len(slack)))
