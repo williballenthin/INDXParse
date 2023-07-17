d87cdf552aad9871527261313d0d5709ef30da0a
Alex Nelson <alexander.nelson@nist.gov>
2023-07-17 09:34:00 -0400

    Update NIST contribution license text
    Signed-off-by: Alex Nelson <alexander.nelson@nist.gov>

--- INDXParse/get_file_info.py
+++ INDXParse/get_file_info.py
@@ -1,18 +1,7 @@
 #! /usr/bin/env python
 
-#   Portions of this file contributed by NIST are governed by the
-#   following statement:
-#
-#   This software was developed at the National Institute of Standards
-#   and Technology by employees of the Federal Government in the course
-#   of their official duties. Pursuant to title 17 Section 105 of the
-#   United States Code this software is not subject to copyright
-#   protection and is in the public domain. NIST assumes no
-#   responsibility whatsoever for its use by other parties, and makes
-#   no guarantees, expressed or implied, about its quality,
-#   reliability, or any other characteristic.
-#
-#   We would appreciate acknowledgement if the software is used.
+#   Alex Nelson, NIST, contributed to this file.  Contributions of NIST
+#   are not subject to US Copyright.
 
 import argparse
 import array
