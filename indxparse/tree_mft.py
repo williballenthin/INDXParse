cc53b38837f15452bcd93015e4953ebf8499ef4b
Alex Nelson <alexander.nelson@nist.gov>
2023-10-17 17:35:52 -0400

    Review tree_mft.py with mypy --strict
    The `Mmap` class in `tree_mft.py` appears to be a redundant and fallen-
    behind implementation of the `Mmap` class in `BinaryParser.py`.  This
    patch uses the central implementation instead, which happens to enable
    `mpy --strict` to pass.
    
    These changes were tested in Pythons 3.8 through 3.12.  No effects were
    observed on Make-managed files.
    
    Disclaimer:
    Participation by NIST in the creation of the documentation of mentioned
    software is not intended to imply a recommendation or endorsement by the
    National Institute of Standards and Technology, nor is it intended to
    imply that any specific software is necessarily the best available for
    the purpose.
    
    Signed-off-by: Alex Nelson <alexander.nelson@nist.gov>

--- indxparse/tree_mft.py
+++ indxparse/tree_mft.py
@@ -5,30 +5,9 @@
 
 import argparse
 import logging
-import mmap
 
+from indxparse.BinaryParser import Mmap
 from indxparse.MFT import Cache, MFTTree, MFTTreeNode
-
-
-class Mmap(object):
-    """
-    Convenience class for opening a read-only memory map for a file path.
-    """
-
-    def __init__(self, filename):
-        super(Mmap, self).__init__()
-        self._filename = filename
-        self._f = None
-        self._mmap = None
-
-    def __enter__(self):
-        self._f = open(self._filename, "rb")
-        self._mmap = mmap.mmap(self._f.fileno(), 0, access=mmap.ACCESS_READ)
-        return self._mmap
-
-    def __exit__(self, type, value, traceback):
-        self._mmap.close()
-        self._f.close()
 
 
 def main() -> None:
